fbe1876eeba9f64cbc896ea6fd0a56a3863009dd
Mahdi TZ <97670594+mahditz@users.noreply.github.com>
2022-08-16 20:12:56 +0430

    Add files via upload

---
+++ d.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+url = 'https://divar.ir/s/varamin/real-estate'
+f = []
+while True :
+    x = requests.get(url)
+    soup = BeautifulSoup(x.content,"html.parser")
+    for file in soup.find_all(class_="waf972") :
+        if file.find(class_='kt-post-card__title').text.find('فجر')!= -1 or file.find(class_='kt-post-card__title').text.find('بذر')!= -1 or file.find(class_='kt-post-card__title').text.find('ولیعصر')!= -1 :
+            if file.find(class_='kt-post-card__title').text not in f :
+                xx = requests.get("https://divar.ir"+file.find("a")["href"])
+                soup2  = BeautifulSoup(xx.content,"html.parser")
+                status = False
+                for onefile in soup2.find_all(class_="kt-unexpandable-row__value") :
+                    if onefile.text.find('شخصی') != -1 :
+                        status = True
+                        break
+                if status == True :
+                    params =  {"username":"mahditz","password":"T0ZWLqyWAGj8qzcRlmASEyr2J5oAgTlccTGdVJZxEpZnBF1hbUXGjk3oJVTZwZyG",
+                            "line":"30007732002955"}
+                    params["mobile"] = "09213707906"
+                    params["text"]  = file.find(class_='kt-post-card__title').text
+                    x = requests.get("https://api.sms.ir/v1/send",params=params)
+                    params["mobile"] = "09303700906"
+                    x = requests.get("https://api.sms.ir/v1/send",params=params)
+                    f.append(file.find(class_='kt-post-card__title').text)
+    if len(f) == 25 :
+        del f[-10:]
+    sleep(900)  
+        
+
+        
+
